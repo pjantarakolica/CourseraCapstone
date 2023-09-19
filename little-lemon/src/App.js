bb162a185b4c3cc2b926f7cb7890f1f0e1cf4d64
Michi Jantarakolica <pjantarakolica@beckershealthcare.onmicrosoft.com>
2023-09-19 09:05:21 -0700

    Add Chakra

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ChakraProvider>
+      <div className="App">
+      </div>
+    </ChakraProvider>
   );
 }
 
