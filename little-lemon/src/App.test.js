25a3e14bfae778bc8eb3515e20dc8dd87e821ae9
Michi Jantarakolica <pjantarakolica@beckershealthcare.onmicrosoft.com>
2023-09-22 00:27:50 -0700

    Add test cases

--- little-lemon/src/App.test.js
+++ little-lemon/src/App.test.js
@@ -1,15 +1,26 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Testimonials from './Components/Testimonials';
+import BookingPage from './BookingPage';
+import { Routes, BrowserRouter } from 'react-router-dom';
 
-test('Renders the BookingForm heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText("Online Booking");
+test('Renders the Testimonial Heading', () => {
+  render(<Testimonials />);
+  const headingElement = screen.getByText("Testimonials");
   expect(headingElement).toBeInTheDocument();
 });
 
+test('Renders the BookingForm heading', () => {
+  render(
+    <BrowserRouter>
+    <BookingPage availableTimes={["17:00"]}/>
+    </BrowserRouter>
+  );
+  const headingElement = screen.getByText("Online Booking");
+  expect(headingElement).toBeInTheDocument();
+})
 
-test('Test initializeTimes function',() => {
-  render(<App />);
-  dispatch({type:'initializeTimes'});
-  expect(availableTimes).toBe([]);
-});
+// test('Test initializeTimes function', () => {
+//   render(<App />);
+//   dispatch({type:'initializeTimes'});
+//   expect(availableTimes).toBe([]);
+// });
